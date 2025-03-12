721907df3254cb46ec9f94f30d6538bad438a209
Yudo <53389941+juniz@users.noreply.github.com>
2025-03-12 15:39:16 +0700

    tambah command CekFinger dan model FingerBpjs serta ReferensiMobilejknBpjs, dan migrasi untuk tabel finger_bpjs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('bpjs:finger')
+            ->everyFiveMinutes()
+            ->between('07:00', '15:00')
+            ->timezone('Asia/Jakarta')
+            ->withoutOverlapping();
     }
 
     /**
@@ -25,7 +29,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
